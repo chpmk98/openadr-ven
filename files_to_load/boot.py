0b13adaa8651e44b92f6b6e81f1c31589857387a
Alvin Tan <alverino@berkeley.edu>
2025-06-27 03:57:25 -0700

    allowing ethernet network option (untested)

--- files_to_load/boot.py
+++ files_to_load/boot.py
@@ -13,19 +13,22 @@ repl_button = machine.Pin(34, machine.Pin.IN, machine.Pin.PULL_UP)
 ## Set up our network connection
 import network
 import utime
-import wifi_credentials
-print("Connecting to Wi-Fi..")
 sta_if = network.WLAN(network.WLAN.IF_STA)
-sta_if.active(True)
-sta_if.connect(wifi_credentials.WIFI_SSID, wifi_credentials.WIFI_PASS)
+if sta_if.isconnected():
+    print("Connected to network!")
+else:
+    print("Connecting to Wi-Fi..")
+    import wifi_credentials
+    sta_if.active(True)
+    sta_if.connect(wifi_credentials.WIFI_SSID, wifi_credentials.WIFI_PASS)
 
-# Wait until we successfully connect to Wi-Fi.
-while not sta_if.isconnected():
-    # If button 1 is pressed, drop to REPL
-    if repl_button.value() == 0:
-        raise Exception("Dropping to REPL (while waiting for Wi-Fi connection)")
-    utime.sleep_ms(100)
-print("Wi-Fi successfully connected!")
+    # Wait until we successfully connect to Wi-Fi.
+    while not sta_if.isconnected():
+        # If button 1 is pressed, drop to REPL
+        if repl_button.value() == 0:
+            raise Exception("Dropping to REPL (while waiting for Wi-Fi connection)")
+        utime.sleep_ms(100)
+    print("Wi-Fi successfully connected!")
 
 ## Check to see if micropython-mdns libraries exist.
 import os
